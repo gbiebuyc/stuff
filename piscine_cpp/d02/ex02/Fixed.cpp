3f0b8b74499a14e347cfc12e8487ee5ef2313ef0
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-02-28 15:35:14 +0100

    stuff

---
+++ piscine_cpp/d02/ex02/Fixed.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbiebuyc <gbiebuyc@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/28 12:45:44 by gbiebuyc          #+#    #+#             */
+/*   Updated: 2020/02/28 12:45:44 by gbiebuyc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <math.h>
+
+Fixed::Fixed(void) : _value(0) {
+    std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(Fixed const &src) {
+    std::cout << "Copy constructor called\n";
+    *this = src;
+}
+
+Fixed::~Fixed(void) {
+    std::cout << "Destructor called\n";
+}
+
+Fixed &Fixed::operator=(Fixed const &rhs) {
+    std::cout << "Assignation operator called\n";
+    _value = rhs.getRawBits();
+    return *this;
+}
+
+Fixed Fixed::operator+(Fixed const &rhs) const {
+    Fixed new = *this;
+    return Fixed(_value + rhs.getRawBits());
+}
+
+std::ostream &operator<<(std::ostream &o, Fixed const &rhs) {
+    o << rhs.toFloat();
+    return o;
+}
+
+int Fixed::getRawBits(void) const {
+    return (_value);
+}
+
+void Fixed::setRawBits(int raw) {
+    std::cout << "setRawBits member function called\n";
+    _value = raw;
+}
+
+const int Fixed::_numFractBits = 8;
+
+Fixed::Fixed(int const i) {
+    std::cout << "Int constructor called\n";
+    _value = i << _numFractBits;
+}
+
+Fixed::Fixed(float const f) {
+    std::cout << "Float constructor called\n";
+    _value = (int)(f * (1 << _numFractBits));
+}
+
+float Fixed::toFloat(void) const {
+    return ((float)_value / (float)(1 << _numFractBits));
+}
+
+int Fixed::toInt(void) const {
+    return (_value >> _numFractBits);
+}
---
+++ piscine_cpp/d02/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbiebuyc <gbiebuyc@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/28 10:50:37 by gbiebuyc          #+#    #+#             */
+/*   Updated: 2020/02/28 15:17:51 by gbiebuyc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) {
+    // Fixed a;
+    // Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    // std::cout << a << std::endl;
+    // std::cout << ++a << std::endl;
+    // std::cout << a << std::endl;
+    // std::cout << a++ << std::endl;
+    // std::cout << a << std::endl;
+    // std::cout << b << std::endl;
+    // std::cout << Fixed::max( a, b ) << std::endl;
+    // return 0;
+
+    std::cout << Fixed(3.14f) + Fixed(2) << std::endl;
+}
