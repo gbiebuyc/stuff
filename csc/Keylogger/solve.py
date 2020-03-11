e320e5adfd2dbfdaaf8f8c0dec12d4d0c182a717
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-03-11 14:59:16 +0100

    stuff

--- csc/Keylogger/solve.py
+++ csc/Keylogger/solve.py
@@ -2,5 +2,11 @@
 
 buf = open('csc-keylogger.exe_200212_200703.dmp', 'rb').read()[0x81f47:]
 print('szMarker: ', buf[:32])
-print(int.from_bytes(buf[32:40], 'little'))
-print(int.from_bytes(buf[40:48], 'little'))
+count = int.from_bytes(buf[36:40], 'little')
+print('iCounter: ', count)
+buf = buf[40:]
+s = ''
+for i in range(count):
+	s += chr(int.from_bytes(buf[i*20:i*20+4], 'little'))
+	print(s)
+# CSC{AZERTYCFOTER123OPC}
