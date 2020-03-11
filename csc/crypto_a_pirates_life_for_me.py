46410042dfabc717460715b2f33ad7ab9d9253d3
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-03-11 03:25:43 +0100

    stuff

---
+++ csc/crypto_a_pirates_life_for_me.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from base64 import b64decode, b64encode
+
+VALID_H = "3F34564B0A1FB3AFE3676911FF990B3127BEC0C2"
+SIGNATURE_SIZE = 256
+ASN = b"0!0\t\x06\x05+\x0e\x03\x02\x1a\x05\x00\x04\x14"
+
+
+def find_invpow(x,n):
+    """Finds the integer component of the n'th root of x,
+    an integer such that y ** n <= x < (y + 1) ** n.
+    """
+    high = 1
+    while high ** n < x:
+        high *= 2
+    low = high//2
+    while low < high:
+        mid = (low + high) // 2
+        if low < mid and mid**n < x:
+            low = mid
+        elif high > mid and mid**n > x:
+            high = mid
+        else:
+            return mid
+    return mid + 1
+
+def solve():
+    i = 0
+    a = b"\x00\x01"
+    a += b"\x00"
+    a += ASN
+    a += bytes.fromhex(VALID_H)
+    m = a + i.to_bytes(SIGNATURE_SIZE - len(a), byteorder='big')
+    x = int.from_bytes(m, 'big')
+    y = find_invpow(x, 3) + 1
+    y = y.to_bytes(256, byteorder='big')
+    y = b64encode(y)
+    print(y)
+
+solve()
