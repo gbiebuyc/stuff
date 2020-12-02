4068b2c253bea6e01f9c5fac8964489150bf9a49
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-12-02 16:36:52 +0100

    stuff

---
+++ adventofcode2020/d01/solve.py
@@ -0,0 +1,17 @@
+nums = [int(n) for n in open('input', 'r').readlines()]
+
+def part1():
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == 2020:
+                return nums[i] * nums[j]
+
+def part2():
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            for k in range(j+1, len(nums)):
+                if nums[i] + nums[j] + nums[k] == 2020:
+                    return nums[i] * nums[j] * nums[k]
+
+print('part 1: ', part1())
+print('part 2: ', part2())
---
+++ adventofcode2020/d02/solve.py
@@ -0,0 +1,17 @@
+ans1=0
+ans2=0
+
+def check(i, c, s):
+    i = int(i) - 1
+    if not 0 <= i < len(s):
+        return False
+    return s[i] == c
+
+for l in open('input'):
+    l = l.replace('-', ' ').replace(':', ' ').split()
+    if int(l[0]) <= l[3].count(l[2]) <= int(l[1]):
+        ans1 += 1
+    if check(l[0], l[2], l[3]) ^ check(l[1], l[2], l[3]):
+        ans2 += 1
+print('part 1: ', ans1)
+print('part 2: ', ans2)
