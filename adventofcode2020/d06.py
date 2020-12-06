576b84b74ee1046ec5a4b961ea1c43d9787a9a59
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-12-06 20:20:52 +0100

    stuff

---
+++ adventofcode2020/d06.py
@@ -0,0 +1,13 @@
+groups = open('input-06.txt').read().split('\n\n')
+ans1=0
+ans2=0
+for group in groups:
+    questions1 = set()
+    questions2 = set('abcdefghijklmnopqrstuvwxyz')
+    for person in group.split():
+        questions1 |= set(person)
+        questions2 &= set(person)
+    ans1 += len(questions1)
+    ans2 += len(questions2)
+print(ans1)
+print(ans2)
