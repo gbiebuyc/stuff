23957fff43b16579eef936d727901e8fa72214e5
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-12-14 07:02:49 +0100

    stuff

--- adventofcode2020/d14.py
+++ adventofcode2020/d14.py
@@ -4,7 +4,7 @@ import math
 from sys import argv
 from aoc_cookie import AOC_COOKIE
 
-YEAR, DAY = 2020, 13
+YEAR, DAY = 2020, 14
 INPUT_FILE = 'input-%02d.txt' % DAY
 EXAMPLE_FILE = 'input-%02d-example.txt' % DAY
 try:
@@ -28,3 +28,44 @@ data = full_data if 'f' in argv[1:] else example
 lines = data.splitlines()
 
 # PART 1
+mask=None
+mem={}
+for l in lines:
+    if 'mask' in l:
+        mask=l.split()[2]
+    else:
+        addr = int(l.split('[')[1].split(']')[0])
+        val = int(l.split()[2])
+        for i in range(36):
+            if mask[i]=='0':
+                val &= ~(1<<(36-1-i))
+            elif mask[i]=='1':
+                val |= (1<<(36-1-i))
+        mem[addr]=val
+print(sum(mem.values()))
+
+
+# PART 2
+mask=None
+mem={}
+for l in lines:
+    if 'mask' in l:
+        mask=l.split()[2]
+    else:
+        addr = int(l.split('[')[1].split(']')[0])
+        val = int(l.split()[2])
+        for i in range(36):
+            if mask[i]=='1':
+                addr |= (1<<(36-1-i))
+        for i in range(2**mask.count('X')):
+            j=0
+            a=addr
+            for k in range(36):
+                if mask[k]=='X':
+                    if (i>>j)&1:
+                        a &= ~(1<<(36-1-k))
+                    else:
+                        a |= (1<<(36-1-k))
+                    j+=1
+            mem[a]=val
+print(sum(mem.values()))
