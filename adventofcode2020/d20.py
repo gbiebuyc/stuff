9e7ce068a8163284840668aacea4bc62f7b3395d
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-12-20 23:01:52 +0100

    stuff

--- adventofcode2020/d20.py
+++ adventofcode2020/d20.py
@@ -1,42 +1,120 @@
 #!/usr/bin/env python3
 from sys import stdin
-import itertools
-import math
 
 tiles={}
 for tile in stdin.read().strip().split('\n\n'):
-    tiles[int(tile[5:9])] = tile.splitlines()[1:]
+    tiles[int(tile[5:9])] = [list(row) for row in tile.splitlines()[1:]]
+firstTileNo, firstTile = list(tiles.items())[0]
+TILE_SZ = len(firstTile)-2
+SIDE_TILES = int(len(tiles)**.5)
+IMAGE_SZ = TILE_SZ * SIDE_TILES
 
-def check(border, other):
-    other_top = other[0]
-    other_bottom = other[-1]
-    other_left = ''.join(row[0] for row in other)
-    other_right = ''.join(row[-1] for row in other)
-    return border in (other_top, other_top[::-1],
-                      other_bottom, other_bottom[::-1],
-                      other_left, other_left[::-1],
-                      other_right, other_right[::-1])
+def get_border(tileNo, border):
+    tile = tiles[tileNo]
+    if border==0:
+        return tile[0]
+    if border==1:
+        return [row[-1] for row in tile]
+    if border==2:
+        return tile[-1][::-1]
+    if border==3:
+        return [row[0] for row in tile][::-1]
+
+def rotate(tile, n):
+    for _ in range(n%4):
+        tile = [list(row) for row in zip(*tile[::-1])]
+    return tile
+
+def flip_vertical(tile): return tile[::-1]
+def flip_horizontal(tile): return [row[::-1] for row in tile]
+
+def check(cur, borderNo, other):
+    border = get_border(cur, borderNo)
+    for otherBorderNo in range(4):
+        otherBorder = get_border(other, otherBorderNo)
+        if border == otherBorder:
+            tiles[other] = rotate(tiles[other], borderNo - otherBorderNo + 2)
+            tiles[other] = flip_vertical(tiles[other]) if borderNo%2 else flip_horizontal(tiles[other])
+            return True
+        if border == otherBorder[::-1]:
+            tiles[other] = rotate(tiles[other], borderNo - otherBorderNo + 2)
+            return True
+    return False
 
 corners=[]
-for cur in tiles:
-    borders = [0,0,0,0]
+borders={}
+topleft=None
+
+def find_borders(cur):
+    global topleft
+    if cur in borders:
+        return
+    borders[cur] = [0,0,0,0]
     for other in tiles:
         if cur == other:
             continue
-        top = tiles[cur][0]
-        bottom = tiles[cur][-1]
-        left = ''.join(row[0] for row in tiles[cur])
-        right = ''.join(row[-1] for row in tiles[cur])
-        if check(top, tiles[other]):
-            borders[0] = 1
-        if check(right, tiles[other]):
-            borders[1] = 1
-        if check(bottom, tiles[other]):
-            borders[2] = 1
-        if check(left, tiles[other]):
-            borders[3] = 1
-    if sum(borders) == 2:
+        for border in range(4):
+            if check(cur, border, other):
+                borders[cur][border] = other
+    if borders[cur].count(0) == 2:
         corners.append(cur)
+        if borders[cur][1] and borders[cur][2]:
+            topleft=cur
+    for b in borders[cur]:
+        if b:
+            find_borders(b)
 
+find_borders(firstTileNo)
 print(f'corners: {corners}')
 print(corners[0] * corners[1] * corners[2] * corners[3])
+
+image = [[None]*IMAGE_SZ for _ in range(IMAGE_SZ)]
+tileY = topleft
+y = 0
+while y < IMAGE_SZ:
+    tileX = tileY
+    x = 0
+    while x < IMAGE_SZ:
+        for yy, row in enumerate(tiles[tileX][1:-1]):
+            for xx, cell in enumerate(row[1:-1]):
+                image[y+yy][x+xx] = cell
+        tileX = borders[tileX][1]
+        x += TILE_SZ
+    tileY = borders[tileY][2]
+    y += TILE_SZ
+image = rotate(image, 1)
+monster = ['                  # ',
+           '#    ##    ##    ###',
+           ' #  #  #  #  #  #   ']
+MONSTER_NUM_CHARS = sum(row.count('#') for row in monster)
+
+num_monsters=0
+for _ in range(4):
+    imageNotFlipped = [row.copy() for row in image]
+    for flip in range(3):
+        for y in range(IMAGE_SZ - len(monster)):
+            for x in range(IMAGE_SZ - len(monster[0])):
+                valid = 1
+                for yy in range(len(monster)):
+                    for xx in range(len(monster[0])):
+                        if monster[yy][xx]=='#' and image[y+yy][x+xx]!='#':
+                            valid=0
+                            break
+                    if not valid:
+                        break
+                if valid:
+                    num_monsters += 1
+        if num_monsters:
+            break
+        if flip == 0:
+            image = flip_vertical(imageNotFlipped)
+        elif flip == 1:
+            image = flip_horizontal(imageNotFlipped)
+        elif flip == 2:
+            image = imageNotFlipped
+    if num_monsters:
+        break
+    image = rotate(image, 1)
+IMAGE_NUM_CHARS = sum(row.count('#') for row in image)
+ans = IMAGE_NUM_CHARS - num_monsters * MONSTER_NUM_CHARS
+print(ans)
