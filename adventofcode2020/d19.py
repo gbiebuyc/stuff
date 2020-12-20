56396d773d05857d6d8219757d7e6298642652ff
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-12-20 09:28:13 +0100

    stuff

--- adventofcode2020/d19.py
+++ adventofcode2020/d19.py
@@ -8,24 +8,18 @@ messages=[]
 for l in lines:
     m = re.match(r'(\d+): (.*)', l)
     if m:
-        rules[int(m.group(1))] = m.group(2)
+        rules[m.group(1)] = m.group(2)
     elif l!='':
         messages.append(l)
-#print(rules)
-#print(messages)
 
-def get_possibilities(rule):
-    rule = rules[rule]
+def createRegex(rule_key):
+    rule = rules[rule_key]
     if rule[0] == '"':
-        return [rule[1]]
-    possibilities=[]
-    for p in rule.split('|'):
-        x=['']
-        for r in p.split():
-            x = [a + b for a in x for b in get_possibilities(int(r))]
-        possibilities += x
-    return possibilities
+        return rule[1]
+    regex = '('
+    for token in rule.split():
+        regex += '|' if token=='|' else createRegex(token)
+    return regex + ')'
 
-#print(get_possibilities(0))
-p = get_possibilities(0)
-print(sum(msg in p for msg in messages))
+r = createRegex('0')+'$'
+print(sum(bool(re.match(r, msg)) for msg in messages))
