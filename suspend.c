c82d863e519faa3d4b1cdbbdc4d1847181389bec
gbiebuyc <gbiebuyc@users.noreply.github.com>
2021-05-07 05:21:35 +0200

    stuff

--- suspend.c
+++ suspend.c
@@ -1,8 +1,15 @@
+//
+// cc file.c -lpowrprof
+//
+
 #include <windows.h>
 #include <powrprof.h>
 #include <stdio.h>
 
 int main() {
+	OpenClipboard(NULL);
+	EmptyClipboard();
+	CloseClipboard();
 	BOOLEAN ret = SetSuspendState(0, 0, 0);
 	printf("SetSuspendState ret: %d\n", ret);
 }
